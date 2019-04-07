62828dfc0057fdff91d44fd0155dca0e3a494967
Your Name <you@example.com>
2019-04-07 12:53:45 -0700

    Practice questions continued

---
+++ src/OlympicRings_Threaded.java
@@ -0,0 +1,7 @@
+public class OlympicRings_Threaded {
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One
+	// robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+		System.out.println(13 ^ 10);
+	}
+}
